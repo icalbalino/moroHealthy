6531ac1facea4ce3ce9973f5ec97c544875c77af
if_balino <balino.ical@gmail.com>
2020-12-12 20:35:55 +0700

    init koneksi

--- src/java/config/Koneksi.java
+++ src/java/config/Koneksi.java
@@ -5,10 +5,32 @@
  */
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author user
  */
 public class Koneksi {
+    private String dbDriver = "com.mysql.jdbc.Driver";
+    private String dbUsername = "kapit20";
+    private String dbPass = "1sampai8";
+    private String dbUrl = "jdbc:mysql://206.189.94.183/morohealthy";
+
+    private Connection conn;
+    
+    public Koneksi(){
+        try {
+            Class.forName(dbDriver);
+            conn = DriverManager.getConnection(dbUrl, dbUsername, dbPass);
+        } catch (Exception e) {
+            conn = null;
+        }
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
     
 }
